003c71a2d3245421a80f955f941520060d6e2b17
Rohan Yadav <yadavrohan869@gmail.com>
2026-01-26 22:38:20 +0530

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 16 13:52:04 2025
+
+@author: 91788
+"""
+
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier,plot_tree
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plot
+
+temp_DF = pd.read_excel("C:/Users/91788/Desktop/Python/Data Science/Decision_tree/Projects/4.Gym_Customer_Churn/gym_customer_churn .xlsx",
+                        sheet_name="weight_of_evidence")
+
+customer_DF = temp_DF.drop("Customer_IDs",axis = 1)
+final_DF = customer_DF.drop("Churn",axis =1)
+target_DF = temp_DF["Churn"]
+
+sample_train,sample_test,target_train,target_test=train_test_split(
+                                    final_DF,target_DF,random_state=42,test_size=0.3,stratify=target_DF)
+
+Decision_tree = DecisionTreeClassifier(
+                min_samples_leaf=150,
+                min_samples_split=30,
+                max_depth=4,
+                criterion="gini")
+
+Decision_tree.fit(sample_train,target_train)
+prediction = Decision_tree.predict(sample_test)
+
+plot.figure(figsize=(24,12))
+plot_tree(Decision_tree,
+          feature_names=final_DF.columns,
+          class_names=["No","Yes"],
+          filled=True,
+          rounded=True,
+          fontsize=10)
+plot.title("Decision Tree For Gym Customer Churn",fontsize=10)
+plot.show()
+
+print(classification_report(target_test,prediction))
+
+important_feature = pd.DataFrame({
+        "Feature_name" : final_DF.columns,
+        "Predicted_Values":Decision_tree.feature_importances_}).sort_values(by="Predicted_Values",ascending=False)
+important_feature.to_excel("Important_Feature.xlsx",index=False)
+
+confusion_matrix = pd.DataFrame({
+            "Customer_IDs":sample_test.index,
+            "Actual_Values":target_test.values,
+            "Predicted_Values":prediction})
+confusion_matrix.to_excel("confusion_matrix.xlsx",index=False)
+
+
+
+
